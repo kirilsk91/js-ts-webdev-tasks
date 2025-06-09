7bd38a4760821a0f5120523caeaa8054cad06abe
Kirils Kravcovs <kravcovs.kirils@student.ehu.lt>
2025-06-09 21:07:29 +0300

    feat: cleanup

--- shop-co/src/main.ts
+++ shop-co/src/main.ts
@@ -1,24 +1,15 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './style.css';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = /*html*/ `
+  <div class="red-container">
+  Red container text
   </div>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  <div class="green-card">
+    Green card content
+  </div>
+
+  <div class="blue-footer">
+    Footer text
+  </div>
+`;
